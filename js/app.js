7778a56e19841ee040dccdae6c835b6c5d8717e8
ALX <alexmonac13@gmail.com>
2023-02-07 15:27:52 +0100

    [ Update ] a redirect animations

--- js/app.js
+++ js/app.js
@@ -3,15 +3,19 @@ const frameworkSection = document.querySelector("#frameworks");
 for (let framework of frameworks) {
   console.log(framework);
 
+  // on capte le nom et on supprime la substring qui commence au .*
+
   const htmlContent = `
-  <article class="card">
+  <article id="${framework.id}" class="card">
   <div id="avds">
   <div id="titre"><h2>${framework.name}</h2>
   <div id="image">${framework.img}</div></div>
   <p class="description">${framework.description}</p>
-  <p>Avantages : ${framework.pros}</p>
+  <p>Avantages : ${framework.pros.join("")}</p>
   <p>Désavantages : ${framework.cons}</p>
-  <div id="button"><a target="_blank" id="link" href="${framework.link}"><button>Lien du framework</button></a></div></div>
+  <div id="button"><a target="_blank" id="link" href="${
+    framework.link
+  }"><button>Lien du framework</button></a></div></div>
   </article>
   `;
 
